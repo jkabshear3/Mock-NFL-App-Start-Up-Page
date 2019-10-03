9de2c1830c0700d1c16ff90fe2edc752d55df926
Jacob <jkabshear3@gmail.com>
2019-10-03 12:38:13 -0400

    Updates

---
+++ precache-manifest.bda45fc047a684d7ba48eca09175d9a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76e5d5a206cdd4c90f0dfd2a0dfaa31",
+    "url": "/Mock-NFL-App-Start-Up-Page/index.html"
+  },
+  {
+    "revision": "d7ac205e799bf4b37b59",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/css/main.de71ad8e.chunk.css"
+  },
+  {
+    "revision": "81c5c1bda7e3a23bff13",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/js/2.fce156e4.chunk.js"
+  },
+  {
+    "revision": "d7ac205e799bf4b37b59",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/js/main.6b97f45c.chunk.js"
+  },
+  {
+    "revision": "6a2856c2d980e129ebb9",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/js/runtime-main.6a289027.js"
+  },
+  {
+    "revision": "0ef663204912a292a9bb4dc9826e3c08",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/media/barkley.0ef66320.jpg"
+  },
+  {
+    "revision": "db5d5e4be133cb0e1d2e98ee52e73c31",
+    "url": "/Mock-NFL-App-Start-Up-Page/static/media/reallogo.db5d5e4b.png"
+  }
+]);
